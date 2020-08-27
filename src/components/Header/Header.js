6e7b046a2e2d87ea26f9e17c21e6d3a9f8816df4
sabirul-islam <67526212+sabirul-islam@users.noreply.github.com>
2020-08-27 18:08:59 +0600

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-center text-primary'> Online Courses </h1>
+            <nav className='bg-success text-white py-2'>
+                <a className='p-4 text-white text-decoration-none' href="/courses">Online Courses</a>
+                <a className='p-4 text-white text-decoration-none' href="/instructor">Our instructor</a>
+                <a className='p-4 text-white text-decoration-none'href="/review">Student's Review</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
